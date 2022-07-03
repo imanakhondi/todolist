305884afb5c27f5b890301547ae055dcf947a3f1
imankd <akhondiiman@gmail.com>
2022-07-03 15:56:09 +0430

    created project

---
+++ src/components/TodoApp/TodoApp.js
@@ -0,0 +1,13 @@
+import TodoForm from "../TodoForm/TodoForm";
+import TodoList from "../TodoList/TodoList";
+
+const TodoApp = () => {
+  return (
+    <div>
+      <TodoForm />
+      <TodoList />
+    </div>
+  );
+};
+
+export default TodoApp;
---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,9 @@
+import Todo from "../Todo/Todo";
+
+const TodoList = () => {
+  return <div>
+    <Todo/>
+  </div>;
+};
+
+export default TodoList;
